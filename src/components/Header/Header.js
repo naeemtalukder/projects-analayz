8697744c5b71af1d8f185e43113043e0e3904fe7
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-04-04 20:26:24 +0600

    navbar done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='sticky top-0 bg-white px-12 py-6 flex justify-center'>
+            <div className='flex text-xl gap-5'>
+                <Link to='/'>HOME</Link>
+                <Link to='/reviews'>REVIEWS</Link>
+                <Link to='/dashboard'>DESHBOARD</Link>
+                <Link to='/blogs'>BLOGS</Link>
+                <Link to='/about'>ABOUT</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='md:px-16 py-8'>
+            <div className='flex flex-col justify-center h-{80vh} items-center'>
+                <h1 className='font-mono text-xl md:text-5xl'>Your next Laptop</h1>
+                <button className='py-2 px-3 text-white mt-5 rounded-full bg-cyan-600'>See All Reviews</button>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
